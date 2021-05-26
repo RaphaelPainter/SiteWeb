9d72efb5b88ecf2a4949299042a56d009387768d
Raphaël Painter <raphapainter@wanadoo.fr>
2021-05-26 18:05:34 +0000

    unfinished refactor(all)

--- main.mjs
+++ main.mjs
@@ -33,6 +33,12 @@ var player_unit
 
 var cursors;
 
+var rythm_bar;
+var rythm_text="";
+var spaces_text="";
+var subTick_idx = 0;
+var numberOfSubTicks = 20;
+
 var game = new Phaser.Game(config);
 
 function preload ()
@@ -43,7 +49,7 @@ function preload ()
 
 function create ()
 {
-    //console.clear();
+    console.clear();
     // grid
     tiles = this.physics.add.staticGroup();
     for(let i = 0;i<grid_height;i++){
@@ -61,40 +67,43 @@ function create ()
 
     //user inputs
     cursors = this.input.keyboard.createCursorKeys();
+    
+    setInterval(tick, 50);
+    rythm_bar = document.getElementById("rythm_bar");
+    for(let i = 0;i<numberOfSubTicks;i++){
+        rythm_text += "⯀"; 
+    }
+}
 
-    /*document.onwheel = zoom;
-    function zoom(event) {
-        if(event.deltaY < 0 && zoom_() < 4.5 ){
-            resizeWorld(zoom_()-event.deltaY/100*0.25, player, tiles);
-        }else if(event.deltaY > 0 && zoom_() > 0.5 ){
-            resizeWorld(zoom_()-event.deltaY/100*0.25, player, tiles);
-        }
-    }*/
+function tick(){
+    subTick_idx++;
+    rythm_bar.innerHTML = 
+    rythm_text.substr(subTick_idx-2,numberOfSubTicks);
+    if(subTick_idx > numberOfSubTicks){
+        move_r(player_unit, input_xaxis(cursors), input_yaxis(cursors));
+        subTick_idx = 1;
+    }
+}
 
-    /*document.addEventListener('keydown', (e) => {
-        //move_r(player_unit, input_xaxis(cursors), input_yaxis(cursors));
-        if(!e.repeat ){
-            if(e.keyCode == 39 ){
-                move_r(player_unit, 1,0);
-            }if(e.keyCode == 37 ){
-                move_r(player_unit, -1,0);
-            }if(e.keyCode == 38 ){
-                move_r(player_unit, 0,-1);
-            }if(e.keyCode == 40 ){
-                move_r(player_unit, 0,1);
-            }
-        }else{
+function update() {
+
+}
+
+document.addEventListener('keydown', (e) => {
+    //move_r(player_unit, input_xaxis(cursors), input_yaxis(cursors));
+    if(!e.repeat ){
+        if(e.keyCode == 39 ){
             move_r(player_unit, 1,0);
+        }if(e.keyCode == 37 ){
+            move_r(player_unit, -1,0);
+        }if(e.keyCode == 38 ){
+            move_r(player_unit, 0,-1);
+        }if(e.keyCode == 40 ){
+            move_r(player_unit, 0,1);
         }
-      });*/
-}
+        subTick_idx = 1;
+    }
 
-function update ()
-{
-    move_r(player_unit, input_xaxis(cursors), input_yaxis(cursors));
-}
-
-
-
+  });
 
 
