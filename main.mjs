1a55d8720a44d98c442f842f18c183080e2d65c6
RaphaelPainter <raphapainter@wanadoo.fr>
2021-10-27 17:06:44 +0200

    feat(rythm): :construction: dosomething when tick

--- main.mjs
+++ main.mjs
@@ -1,25 +1,24 @@
-
-import { 
+import {
     new_,
-} from './modules/misc.mjs'; 
+} from './modules/misc.mjs';
 
-import { 
+import {
     rythm_activated,
     rythm_bar_displayed,
-    rythm_breakable, 
-    time_between_ticks, 
+    rythm_breakable,
+    time_between_ticks,
     event_rythmBreakable,
     init_rythm_var,
     tick_withRythmBar,
     tick_noRythmBar,
-} from './modules/rythm.mjs'; 
+} from './modules/rythm.mjs';
 
-import { 
-    grid_width, 
-    grid_height, 
-    move_a, 
+import {
+    grid_width,
+    grid_height,
+    move_a,
     zoom_
-} from './modules/grid.mjs'; 
+} from './modules/grid.mjs';
 
 
 var config = {
@@ -52,73 +51,66 @@ var rythm = {};
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload() {
     this.load.image('tile', 'assets/tile.png');
     this.load.image('player', 'assets/player.png');
     this.load.image('mob', 'assets/mob.png');
 
 }
 
-function create ()
-{
+function create() {
     console.clear();
-    
+
     // grid
     tiles = this.physics.add.staticGroup();
-    for(let i = 0;i<grid_height;i++){
-        for(let j = 0;j<grid_width;j++){
-           let tile = new_('tile',tiles);
-           move_a(tile, i, j);
+    for (let i = 0; i < grid_height; i++) {
+        for (let j = 0; j < grid_width; j++) {
+            let tile = new_('tile', tiles);
+            move_a(tile, i, j);
         }
     }
 
     //player
     units = this.physics.add.staticGroup();
-    player_unit = units.create(0,0,'player');
+    player_unit = units.create(0, 0, 'player');
     player_unit.setScale(zoom_());
+    player_unit.type = "player"
+    player_unit.id = "1"
     move_a(player_unit, 0, 0);
 
     //user inputs
     cursors = this.input.keyboard.createCursorKeys();
-    
+
     //setup tick calls
     rythm.subTick = {};
     rythm.movedSinceLastTick = false;
 
-    if(rythm_activated){
-        if(rythm_bar_displayed){
+    if (rythm_activated) {
+        if (rythm_bar_displayed) {
             init_rythm_var(rythm);
-            setInterval(()=>{
-                tick_withRythmBar(rythm, cursors,player_unit)
-            } ,time_between_ticks);
-        }else{
-            setInterval(()=>{
+            setInterval(() => {
+                tick_withRythmBar(rythm, cursors, player_unit)
+            }, time_between_ticks);
+        } else {
+            setInterval(() => {
                 tick_noRythmBar(rythm, cursors, player_unit)
             }, time_between_ticks);
         }
     }
-    if(rythm_breakable){
+    if (rythm_breakable) {
         event_rythmBreakable(document, player_unit, rythm.subTick);
     }
 
     //generating MAP
     //mobs
-    let unit = units.create(0,0,'mob');
+    let unit = units.create(0, 0, 'mob');
     mobs.push(unit);
     unit.setScale(zoom_());
+    unit.type = "mob"
+    unit.id = "2"
     move_a(unit, 5, 5);
 }
-
-
 
 function update() {
 
 }
-
-
-
-
-
-
-
--- modules/rythm.mjs
+++ modules/rythm.mjs
@@ -1,21 +1,21 @@
-import { 
+import {
     input_xaxis,
     input_yaxis
-} from './inputs.mjs'; 
+} from './inputs.mjs';
 
 import {
     move_r
-} from './grid.mjs'; 
+} from './grid.mjs';
 
-import { 
+import {
     log
-} from './log.mjs'; 
+} from './log.mjs';
 import { apply } from './tools.mjs';
 
-import{
+import {
     units,
     cursors
-}from '../main.mjs'; 
+} from '../main.mjs';
 
 
 // RYTHM CONGIG
@@ -27,73 +27,82 @@ export var rythm_char = '⯀';
 export var time_between_ticks = 20;
 
 //RYTHM
-export function event_rythmBreakable(document, player_unit, subTick){
+export function event_rythmBreakable(document, player_unit, subTick) {
     document.addEventListener('keydown', (e) => {
-        if(!e.repeat ){
-            if(e.keyCode == 39 ){
-                move_r(player_unit, 1,0);
-            }if(e.keyCode == 37 ){
-                move_r(player_unit, -1,0);
-            }if(e.keyCode == 38 ){
-                move_r(player_unit, 0,-1);
-            }if(e.keyCode == 40 ){
-                move_r(player_unit, 0,1);
+        if (!e.repeat) {
+            if (e.keyCode == 39) {
+                move_r(player_unit, 1, 0);
             }
-            subTick.idx = 1;  
-            if(rythm_breakable){
+            if (e.keyCode == 37) {
+                move_r(player_unit, -1, 0);
+            }
+            if (e.keyCode == 38) {
+                move_r(player_unit, 0, -1);
+            }
+            if (e.keyCode == 40) {
+                move_r(player_unit, 0, 1);
+            }
+            subTick.idx = 1;
+            if (rythm_breakable) {
                 tick();
-            } 
+            }
         }
-  });
+    });
 }
-export function init_rythm_var(rythm){
+export function init_rythm_var(rythm) {
     rythm.bar = document.getElementById("rythm_bar");
-    rythm.text="";
-    for(let i = 0;i<numberOfTicks;i++){
-        rythm.text += rythm_char; 
+    rythm.text = "";
+    for (let i = 0; i < numberOfTicks; i++) {
+        rythm.text += rythm_char;
     }
-  }
-  export function tick_withRythmBar(rythm,player_unit){
+}
+export function tick_withRythmBar(rythm, player_unit) {
     rythm.subTick.idx++;
-    rythm.bar.innerHTML = 
-    rythm.text.substr(rythm.subTick.idx-2,numberOfTicks);
-    if(!rythm.movedSinceLastTick){
-        if( input_xaxis(cursors) != 0 || input_yaxis(cursors) != 0){
-            if(!rythm_breakable){
+    rythm.bar.innerHTML =
+        rythm.text.substr(rythm.subTick.idx - 2, numberOfTicks);
+    if (!rythm.movedSinceLastTick) {
+        if (input_xaxis(cursors) != 0 || input_yaxis(cursors) != 0) {
+            if (!rythm_breakable) {
                 move_r(player_unit, input_xaxis(cursors), input_yaxis(cursors));
                 tick();
             }
             rythm.movedSinceLastTick = true;
         }
     }
-    if(rythm.subTick.idx > numberOfTicks){
+    if (rythm.subTick.idx > numberOfTicks) {
         rythm.subTick.idx = 1;
         rythm.movedSinceLastTick = false;
     }
 }
-export function tick_noRythmBar(rythm,player_unit){
+export function tick_noRythmBar(rythm, player_unit) {
     rythm.subTick.idx++;
-    if(!rythm.movedSinceLastTick){
-        if( input_xaxis(cursors) != 0 || input_yaxis(cursors) != 0){
-            if(!rythm_breakable){
+    if (!rythm.movedSinceLastTick) {
+        if (input_xaxis(cursors) != 0 || input_yaxis(cursors) != 0) {
+            if (!rythm_breakable) {
                 move_r(player_unit, input_xaxis(cursors), input_yaxis(cursors));
                 tick();
             }
             rythm.movedSinceLastTick = true;
         }
     }
-    if(rythm.subTick.idx > numberOfTicks){
+    if (rythm.subTick.idx > numberOfTicks) {
         rythm.subTick.idx = 1;
         rythm.movedSinceLastTick = false;
     }
 }
-export function tick(){
+export function tick() {
     //TODO: units -> tick -> units.behavior -> units.play()
-    
+    //TODO: distinct player and units
+    console.clear();
     apply(units, dosomething, "blarg")
 }
 
-function dosomething(unit, value){
-    log(unit.x +" "+ value);
-}
+function dosomething(unit, value) {
 
+    //TODO: log mod and log level
+    //TODO:  IA
+    if (unit.type != "player") {
+        move_r(unit, -1, -1)
+    }
+    log("id: " + unit.id + " type:" + unit.type + " x:" + unit.idx + " y:" + unit.idy);
+}
